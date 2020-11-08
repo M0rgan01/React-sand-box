74d229b73d3f8ff9a2598ce6097093e10671898d
Morgan <pichat.morgan@gmail.com>
2020-11-08 15:34:32 +0100

    Finalisation du tuto

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,15 @@
 import React from 'react';
 import logo from './logo.svg';
+import {Clock} from './components/Clock';
+import DeleteIcon from '@material-ui/icons/Delete';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+} from "react-router-dom";
 import './App.css';
+import { Button } from '@material-ui/core';
 
 function App() {
   return (
@@ -19,7 +28,59 @@ function App() {
           Learn React
         </a>
       </header>
+
+      <Router>
+        <Button startIcon={<DeleteIcon />} component={Link} to="/" color="secondary" variant="contained">
+          Test
+        </Button>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+          <li>
+            <Link to="/dashboard">Dashboard</Link>
+          </li>
+        </ul>
+        <Switch>
+          <Route exact path="/">
+            <Clock/>
+          </Route>
+          <Route path="/about">
+            <About />
+          </Route>
+          <Route path="/dashboard">
+            <Dashboard />
+          </Route>
+        </Switch>
+      </Router>
     </div>
+  );
+}
+
+function Home() {
+  return (
+      <div>
+        <h2>Home</h2>
+      </div>
+  );
+}
+
+function About() {
+  return (
+      <div>
+        <h2>About</h2>
+      </div>
+  );
+}
+
+function Dashboard() {
+  return (
+      <div>
+        <h2>Dashboard</h2>
+      </div>
   );
 }
 
